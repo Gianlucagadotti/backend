b92e989dc9e5c37921bb2126c9505028a454dd4c
Gianlucagadotti <gianlucagadotti@gmail.com>
2025-07-01 16:27:03 -0300

    Início projeto

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gestao',
     'rest_framework',
-    
+
 ]
 
 MIDDLEWARE = [
